caa705e8114d1962e51f64638c833a21a3a908e4
mukes137 <t4.cs.soft9@gmail.com>
2023-05-17 19:57:43 +0545

    understanding the basic flow of python

---
+++ Introduction /day1.py
@@ -0,0 +1,12 @@
+# VARIABLES IN PYTHON
+
+number1  = 10            # numbe1 is the varibale ofinteger type
+number2  = 10.11         # number2 is the varibale of floating point type 
+name     = 'alexa'       # name is the variable of string type
+
+# CHANGING THE TYPES OF VARIABLE
+
+print('the initial type of the varibale number is:', type(number1))
+print('using str keywrod we are converting the type of the number varibale to:', type(str(number1)))
+
+
---
+++ Introduction /day2.py
@@ -0,0 +1,18 @@
+# USE OF INPUT FUNCTION 
+
+name = input("Enter your name")
+print('My name is', name )
+
+# DAY 2 TASK 1
+
+first_name = input ('Enter your first name')
+second_name = input ('Enter you second name')
+print('full name:', first_name, second_name)
+
+# DAY 2 TASK 2
+
+number1 = float(input('Enter the first number'))
+number2 = float(input('Enter the second number'))
+
+result = number1 + number2
+print(result**2)
